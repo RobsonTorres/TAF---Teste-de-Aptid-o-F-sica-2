0e491fc87ec8e9374a2aa10e78c9947e5aca59cb
Robson Torres Costa <robsoncosta1234567@gmail.com>
2024-06-01 21:19:32 -0300

    Tarefa 1 - Acrescentando declaração const/let nas variáveis

---
+++ script.js
@@ -0,0 +1,32 @@
+// Variáveis para aplicar os resultados do teste. Você deverá categoriza-las como let/const, mas não deve alterar os valores.
+const gender = process.argv[2]; // Genero 
+const height = process.argv[3]; // Altura
+const barReps = process.argv[4]; // Repetições com barra
+const barSeconds = process.argv[5]; // Tempo das repetições com barra 
+const abs = process.argv[6]; // Abdominais
+const runDistance = process.argv[7]; // Distância da corrida
+const runTime = process.argv[8]; // Tempo da corrida
+const swimDistance = process.argv[9]; // Distância da natação
+const swimTime = process.argv[10]; // Tempo da natação
+const diveTime = process.argv[11]; // Tempo de mergulho
+
+let passed = false;
+
+/*
+    Importante:
+    Se o genero (gender) for masculino, utilize a palavra "male"
+    Se o genero (gender) for feminino, utilize a palavra "female"
+
+    Sugestão para começar:
+    if (gender === "male")
+    ...
+*/
+// **sua lógica a partir daqui**
+
+
+
+/*
+    Seu código deve conter apenas UM console.log, e ele deve ser o abaixo.
+    Não altere nada pra baixo dessa linha, senão os testes não irão funcionar.
+*/
+console.log(passed);
